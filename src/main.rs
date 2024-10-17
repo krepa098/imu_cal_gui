44820a0bc295b9089c80e24a2c5bca2c61901094
krepa098 <krepa098@gmail.com>
2024-10-17 16:48:12 +0200

    fix crash on exit

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use std::time::Duration;
+
 mod cal;
 mod ros_node;
 mod ui;
-
-use tokio::time::Duration;
 
 fn main() {
     let rt = tokio::runtime::Runtime::new().expect("Unable to create Runtime");
     let _enter = rt.enter();
 
+    let (_rn, mut node, imu_rx, mag_rx) = ros_node::Node::new();
+
     std::thread::spawn(move || {
         rt.block_on(async {
             loop {
-                tokio::time::sleep(Duration::from_secs(3600)).await;
+                node.spin_once(Duration::from_millis(100));
             }
         })
-    });
-
-    let (_rn, mut node, imu_rx, mag_rx) = ros_node::Node::new();
-
-    let _ros_task = tokio::task::spawn_blocking(move || loop {
-        node.spin_once(std::time::Duration::from_millis(1));
     });
 
     ui::init(imu_rx, mag_rx).unwrap();
